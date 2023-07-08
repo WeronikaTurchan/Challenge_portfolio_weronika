16845dd6934c07221dd63030f1d97e199926ebe9
WeronikaTurchan <turchanweronika@gmail.com>
2023-07-08 14:54:51 +0200

    second_task

--- pages/Add_a_match_form.py
+++ pages/Add_a_match_form.py
@@ -1,16 +1,16 @@
 from pages.base_page import BasePage
 
 
-class AddAMatchForm (BasePage);
+class AddAMatchForm (BasePage):
 
 Date_xpath = "//*[@name='date']"
-CheckBox_xpath = "//*[@class="MuiFormGroup-root"]"
-CheckBox_circle_xpath = "//*[@class="MuiIconButton-label"]/div"
-Submit_button_xpath = "//*[@class="MuiCardActions-root MuiCardActions-spacing"]/button[1]"
-Clear_button_xpath = "//*[@class="MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedSecondary"]"
-Matches_xpath = "//*[@class="MuiListItemText-root jss35 jss214"]"
+CheckBox_xpath = "//*[@class='MuiFormGroup-root']"
+CheckBox_circle_xpath = "//*[@class='MuiIconButton-label']/div"
+Submit_button_xpath = "//*[@class='MuiCardActions-root MuiCardActions-spacing']/button[1]"
+Clear_button_xpath = "//*[contains(@class, 'MuiButton-containedSecondary')]"
+Matches_xpath = "//*/ul[2]/div[2]/div[2]/span"
 My_team_xpath = "//*[text()='My team']"
 Enemy_team_xpath = "//*[text()='Enemy team']"
 Time_Played_xpath = "//*[text()='Time played']"
-Tshirt_color_xpath = "//*[@name='tshirt'] "
+Tshirt_color_xpath = "//*[@name='tshirt']"
 pass
--- pages/dashboard.py
+++ pages/dashboard.py
@@ -3,16 +3,16 @@ from pages.base_page import BasePage
 
 class Dashboard(BasePage):
     button_xpath = "//*[@id='login']"
-    Main_page_xpath = "//*[@class="MuiListItemText-root jss29 jss30"]/span"
-    Players_xpath = "//*[@class="MuiListItemText-root jss29 jss31"]/span"
+    Main_page_xpath = "//*[@class='MuiListItemText-root jss29 jss30']/span"
+    Players_xpath = "//*[@class='MuiListItemText-root jss29 jss31']/span"
     Language_xpath = "//*[@class='MuiListItemText-root']/span"
     Sign_out_xpath = "//*[text()='Sign out']"
-    Add_player_xpath = //*[text()='Add player']
+    Add_player_xpath = "//*[text()='Add player']"
     Activity_xpath = "//*[text()='Activity']"
     Scouts_panel_xpath = "//*[text()='Scouts Panel']"
     Shortcuts_xpath = "//*[text()='Shortcuts']"
     Players_count_xpath = "//*[text()='Players count']"
     Events_count_xpath = "//*[text()='Events count']"
-    Description_xpath = "//*[@class="MuiTypography-root MuiTypography-body2 MuiTypography-colorTextSecondary"]"
+    Description_xpath = "//*[@class='MuiTypography-root MuiTypography-body2 MuiTypography-colorTextSecondary']"
 
     pass
